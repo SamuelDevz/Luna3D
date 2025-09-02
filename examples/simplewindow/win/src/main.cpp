85838f5f5add14b7a1f338f3ec40413f97c98b07
Samuel <samuel4512ss@gmail.com>
2025-09-01 23:18:44 -0300

    :sparkles: feat: Adicionando tratamento de exceções no exemplo simplewindow

--- examples/simplewindow/win/src/main.cpp
+++ examples/simplewindow/win/src/main.cpp
@@ -8,6 +8,34 @@ auto APIENTRY WinMain(_In_ HINSTANCE hInstance,
 {
     using namespace Luna;
 
+#ifdef _DEBUG
+
+    try
+    {
+        Engine * engine = new Engine();
+        engine->window->Mode(WINDOWED);
+        engine->window->Size(800, 600);
+        engine->window->Color(0, 122, 204);
+        engine->window->Title("Window Game");
+        engine->window->Icon(IDI_ICON);
+        engine->window->Cursor(IDC_CURSOR);
+        engine->window->LostFocus(Engine::Pause);
+        engine->window->InFocus(Engine::Resume);
+
+        int exit = engine->Start(new WinGame());
+
+        delete engine;
+
+        return exit;
+    }
+    catch (const Error & e)
+    {
+        MessageBox(nullptr, e.ToString().c_str(), "Window Game", MB_OK);
+        return 0;
+    }
+
+#else
+
     Engine * engine = new Engine();
     engine->window->Mode(WINDOWED);
     engine->window->Size(800, 600);
@@ -15,10 +43,14 @@ auto APIENTRY WinMain(_In_ HINSTANCE hInstance,
     engine->window->Title("Window Game");
     engine->window->Icon(IDI_ICON);
     engine->window->Cursor(IDC_CURSOR);
+    engine->window->LostFocus(Engine::Pause);
+    engine->window->InFocus(Engine::Resume);
 
     int exit = engine->Start(new WinGame());
 
     delete engine;
 
     return exit;
+
+#endif
 }
