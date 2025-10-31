88a7c9f71e28e992c973ba80259f352d6678599e
Samuel <samuel4512ss@gmail.com>
2025-10-30 22:13:49 -0300

    :wrench: chore: Remove o warning C4251 do MSVC

--- src/win/include/Export.h
+++ src/win/include/Export.h
@@ -2,13 +2,15 @@
 
 #if !defined(ENGINE_EXPORT)
     #define DLL
-#else 
+#else
     #if defined(_WIN32)
         #if defined(ENGINE_EXPORT)
             #define DLL __declspec(dllexport)
         #else
             #define DLL __declspec(dllimport)
         #endif
+        
+        #pragma warning(disable: 4251)
     #elif defined(__GNUC__) && defined(ENGINE_EXPORT)
         #define DLL __attribute__((visibility("default")))
     #endif
