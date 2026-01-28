c0954b39ff3148bf59b1a637426f8b35d3cf5dcd
Samuel <samuel4512ss@gmail.com>
2026-01-28 13:52:56 -0300

    :sparkles: feat: Adicionar suporte a movimento, botões e scroll do mouse

--- src/linux/Wayland/include/Input.h
+++ src/linux/Wayland/include/Input.h
@@ -14,9 +14,14 @@ namespace Luna
     private:
         static wl_seat* seat;
         static wl_keyboard* keyboard;
+        static wl_pointer* pointer;
 
         static bool keys[MAX_KEYS];
         static bool ctrl[MAX_KEYS];
+
+        static int32 mouseX;
+        static int32 mouseY;
+        static int16 mouseWheel;
         
         static xkb_context* context;
         static xkb_keymap* keymap;
@@ -33,6 +38,21 @@ namespace Luna
         static void HandleKeyboardModifiers(void *userData, wl_keyboard *keyboard, 
             uint32 serial, uint32 dep, uint32 lat, uint32 loc, uint32 grp);
 
+        static void HandlePointerEnter(void* userData, wl_pointer* pointer, 
+            uint32 serial, wl_surface* surface, wl_fixed_t sx, wl_fixed_t sy);
+        
+        static void HandlePointerLeave(void* userData, wl_pointer* pointer, 
+            uint32 serial, wl_surface* surface);
+        
+        static void HandlePointerMotion(void* userData, wl_pointer* pointer, 
+            uint32 time, wl_fixed_t sx, wl_fixed_t sy);
+        
+        static void HandlePointerButton(void* userData, wl_pointer* pointer, 
+            uint32 serial, uint32 time, uint32 button, uint32 state);
+        
+        static void HandlePointerAxis(void* userData, wl_pointer* pointer, 
+            uint32 time, uint32 axis, wl_fixed_t value);
+
         static void SeatHandleCapabilities(void *userData, wl_seat *seat, 
             uint32 caps);
 
@@ -47,6 +67,10 @@ namespace Luna
         bool KeyDown(const uint32 vkcode) noexcept;
         bool KeyUp(const uint32 vkcode) noexcept;
         bool KeyPress(const uint32 vkcode) noexcept;
+
+        int32 MouseX() const noexcept;
+        int32 MouseY() const noexcept;
+        int16 MouseWheel() noexcept;
     };
 
     inline bool Input::KeyDown(const uint32 vkcode) noexcept
@@ -54,4 +78,10 @@ namespace Luna
 
     inline bool Input::KeyUp(const uint32 vkcode) noexcept
     { return !keys[KeysymToKeycode(vkcode)]; }
+
+    inline int32 Input::MouseX() const noexcept
+    { return mouseX; }
+
+    inline int32 Input::MouseY() const noexcept
+    { return mouseY; }
 }
