0995f64b427aa4ae22f4701e216629e80552afde
Samuel <samuel4512ss@gmail.com>
2026-02-01 00:00:40 -0300

    :recycle: refactor: Refatorar classe Timer de alta precisão para todas APIs

--- src/linux/Xlib/src/Timer.cpp
+++ src/linux/Xlib/src/Timer.cpp
@@ -4,38 +4,32 @@ namespace Luna
 {
     Timer::Timer() noexcept : start{}, end{}, stopped{false} 
     {
-        clock = CLOCK_MONOTONIC;
         freq = 1000000000;
-
+        clock_id = CLOCK_MONOTONIC;
         timespec value;
+        
         if (clock_gettime(CLOCK_MONOTONIC_RAW, &value) == 0)
-            clock = CLOCK_MONOTONIC_RAW;
+            clock_id = CLOCK_MONOTONIC_RAW;
+
+        Start();
     }
 
     void Timer::Start() noexcept
     {
         if (stopped)
         {
-            // resumes time counting
-            //
-            //      <--- elapsed ---->
-            // ----|------------------|------------> time
-            //    start               end     
-            //
+            int64 elapsedNs = GetNanoseconds(end) - GetNanoseconds(start);
+            clock_gettime(clock_id, &start);
 
-            int64 elapsed = (end.tv_sec * freq + end.tv_nsec) - (start.tv_sec * freq + start.tv_nsec);
-            clock_gettime(clock, &start);
-
-            int64 startTimeNs = start.tv_sec * freq + start.tv_nsec;
-            startTimeNs -= elapsed;
+            int64 newStartNs = GetNanoseconds(start) - elapsedNs;
             
-            start.tv_sec = startTimeNs / freq;
-            start.tv_nsec = startTimeNs % freq;
+            start.tv_sec = newStartNs / freq;
+            start.tv_nsec = newStartNs % freq;
             stopped = false;
         }
         else
         {
-            clock_gettime(clock, &start);
+            clock_gettime(clock_id, &start);
         }
     }
 
@@ -43,62 +37,34 @@ namespace Luna
     {
         if (!stopped)
         {
-            clock_gettime(clock, &end);
+            clock_gettime(clock_id, &end);
             stopped = true;
         }
     }
 
     float Timer::Reset() noexcept
     {
-        int64 elapsed{};
-
-        if (stopped)
-        {
-            elapsed = (end.tv_sec * freq + end.tv_nsec) - (start.tv_sec * freq + start.tv_nsec);
-            clock_gettime(clock, &start); 
-            stopped = false;
-        }
-        else
-        {
-            clock_gettime(clock, &end);
-            elapsed = (end.tv_sec * freq + end.tv_nsec) - (start.tv_sec * freq + start.tv_nsec);
-            start = end;
-        }
-
-        return static_cast<float>(elapsed / double(freq));
+        float elapsed = Elapsed();
+        start = end; 
+        stopped = false;
+        return elapsed;
     }
 
     float Timer::Elapsed() noexcept
     {
-        int64 elapsed{};
-
-        if (stopped)
-        {
-            elapsed = (end.tv_sec * freq + end.tv_nsec) - (start.tv_sec * freq + start.tv_nsec);
-        }
-        else
-        {
-            clock_gettime(clock, &end);
-            elapsed = (end.tv_sec * freq + end.tv_nsec) - (start.tv_sec * freq + start.tv_nsec);
-        }
-
-        return static_cast<float>(elapsed / double(freq));
+        if (!stopped) 
+            clock_gettime(clock_id, &end);
+        
+        int64 elapsedNs = GetNanoseconds(end) - GetNanoseconds(start);
+        return static_cast<float>(elapsedNs / static_cast<double>(freq));
     }
 
     float Timer::Elapsed(const int64 stamp) noexcept
     {
-        int64 elapsed{};
-
-        if (stopped)
-        {
-            elapsed = (end.tv_sec * freq + end.tv_nsec) - stamp;
-        }
-        else
-        {
-            clock_gettime(clock, &end);
-            elapsed = (end.tv_sec * freq + end.tv_nsec) - stamp;
-        }
-
-        return static_cast<float>(elapsed / double(freq));
+        if (!stopped) 
+            clock_gettime(clock_id, &end);
+        
+        int64 elapsedNs = GetNanoseconds(end) - stamp;
+        return static_cast<float>(elapsedNs / static_cast<double>(freq));
     }
 }
