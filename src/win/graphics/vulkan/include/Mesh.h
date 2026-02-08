75702a8c0bb9207cd206bb6a008326fe82670dd1
Samuel <samuel4512ss@gmail.com>
2026-02-08 16:37:47 -0300

    :sparkles: feat: Adicionar o registro Mesh na engine

---
+++ src/win/graphics/vulkan/include/Mesh.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Types.h"
+#include <vulkan/vulkan_core.h>
+
+namespace Luna
+{
+    struct StagingBuffer
+    {
+        VkBuffer buffer;
+        VkDeviceMemory memory;
+    };
+    
+    struct Mesh
+    {
+        string id;
+
+        int32 vertexCount;
+        VkBuffer vertexBuffer;
+        VkDeviceMemory vertexBufferMemory;
+
+        StagingBuffer vertexUploadBuffer;
+
+	    VkDevice device;
+
+        Mesh(const string_view name) noexcept;
+        ~Mesh() noexcept;
+    };
+}
---
+++ src/win/graphics/vulkan/src/Mesh.cpp
@@ -0,0 +1,32 @@
+#include "Mesh.h"
+
+namespace Luna
+{
+    Mesh::Mesh(const string_view name) noexcept
+        : id{name},
+        vertexCount{},
+        vertexUploadBuffer{nullptr, nullptr},
+        vertexBuffer{nullptr},
+        vertexBufferMemory{nullptr},
+        device{nullptr}
+    {
+    }
+    
+    Mesh::~Mesh() noexcept
+    {
+        if (device)
+        {
+            if (vertexBuffer)
+                vkDestroyBuffer(device, vertexBuffer, nullptr);
+            
+            if (vertexBufferMemory)
+                vkFreeMemory(device, vertexBufferMemory, nullptr);
+            
+            if (vertexUploadBuffer.buffer)
+                vkDestroyBuffer(device, vertexUploadBuffer.buffer, nullptr);
+            
+            if (vertexUploadBuffer.memory)
+                vkFreeMemory(device, vertexUploadBuffer.memory, nullptr);
+        }
+    }
+}
