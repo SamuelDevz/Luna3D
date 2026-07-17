4f08bc2ee63c05b3ff1dd0602932fae21e38f4db
Samuel <samuel4512ss@gmail.com>
2026-07-17 13:33:00 -0300

    :sparkles: feat: Adicionando a função MessageBox e a classe Error na engine

--- src/linux/Xlib/include/All.h
+++ src/linux/Xlib/include/All.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "Types.h"
+#include "Error.h"
+#include "MessageBox.h"
 #include "Window.h"
 #include "Game.h"
 #include "Engine.h"
---
+++ src/linux/Xlib/src/Error.cpp
@@ -0,0 +1,34 @@
+#include "Error.h"
+#include <X11/Xlib.h>
+#include <format>
+using std::format;
+
+namespace Luna
+{
+    Error::Error() noexcept : hrCode{}, lineNum{-1}
+    {
+    }
+
+    Error::Error(Display * display,
+        const int32 hr, 
+        const string_view func, 
+        const string_view file, 
+        const int32 line,
+        const string_view message) noexcept
+        : display{display}, hrCode{hr}, funcName{func}, lineNum{line}, message{message}
+    {
+        auto pos = file.find_last_of('\\');
+
+        if (pos != string::npos)
+            fileName = file.substr(pos + 1);
+    }
+
+    string Error::ToString()
+    {
+        char buffer[1024];
+        XGetErrorText(display, hrCode, buffer, sizeof(buffer));
+
+        return format("{} failed in {}, line {}:\n{}\n{}",
+            funcName, fileName, lineNum, buffer, message);
+    }
+}
